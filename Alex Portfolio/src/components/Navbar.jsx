db208de0ddb0cea63755c548d4ae8f55af4b3d1c
atengelsgjerd <atengelsgjerd@gmail.com>
2024-08-01 11:10:06 -0500

    added initial files for project

---
+++ Alex Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+
+
+
+function Navbar() {
+  return (
+    <>
+    <nav className="navbar">
+        <div className="navbar-container">
+           
+                Alex 
+           
+        </div>
+    </nav>
+    </>
+  );
+}
+
+export default Navbar;
